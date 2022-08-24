bb3e504098f09bdd4998efb67695532007d118d3
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-08-24 16:38:26 +0200

    cfs

--- distilbert/dataloader.py
+++ distilbert/dataloader.py
@@ -1,4 +1,13 @@
 from datasets import load_dataset
-folder_path = "datasets/wikidata-simplequestions-master/"
+import argparse
+
+
+folder_path = "data/wikidata-simplequestions-master/"
+parser = argparse.ArgumentParser(description='Process some env variables')
+parser.add_argument('--path', type = str,
+                    help='add folder path', default = folder_path)
+
+args = parser.parse_args()
+folder_path = args.path
 dataset = load_dataset("text", data_files={"train": folder_path+"annotated_wd_data_train_answerable.txt", "test": folder_path+"annotated_wd_data_test_answerable.txt"}, sample_by="paragraph")
 print(dataset[0])
